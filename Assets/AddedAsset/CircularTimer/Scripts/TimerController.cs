b8c0ba9b23d4b448b75df942f9912d8fe587f173
arriusz97 <65055189+arriusz97@users.noreply.github.com>
2021-02-01 12:58:56 +0900

    인벤토리 구현
    1. 구매한 에셋들 AddedAssets 폴더를 만들어 그안에 모음
    2. UI 틀 구현(예정)
    3. 인벤토리 시스템 구현(예정)

---
+++ Assets/AddedAsset/CircularTimer/Scripts/TimerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eTimerType
+{
+    HP,
+    TP,
+}
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField]
+    private CircularTimer mHP, mTP;
+
+    private CircularTimer mUpdateTimer;
+
+    //타이머 시작
+    private void StartTimer(int TimerType)
+    {
+        if (TimerType == 0)
+        {
+            mHP.StartTimer();
+        }
+        else if (TimerType == 1)
+        {
+            mTP.StartTimer();
+        }
+    }
+
+    private void PauseTimer(int TimerType)
+    {
+        if(TimerType == 0)
+        {
+            mHP.PauseTimer();
+        }
+        else if (TimerType == 1)
+        {
+            mTP.PauseTimer();
+        }
+    }
+
+    //
+    private void UpdateTimer(int TimerType, float Update)
+    {
+        if(TimerType == 0)
+        {
+            mUpdateTimer = mHP;
+        }
+        else if(TimerType == 1)
+        {
+            mUpdateTimer = mTP;
+        }
+        mUpdateTimer.CurrentTime = Mathf.Clamp(mUpdateTimer.CurrentTime - Update, 0, mUpdateTimer.duration);
+    }
+
+    private void Start()
+    {
+        StartTimer((int)eTimerType.HP);
+        StartTimer((int)eTimerType.TP);
+    }
+}
