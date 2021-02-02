08734217196808f7afc436f207f8e06bcaebbf69
arriusz97 <65055189+arriusz97@users.noreply.github.com>
2021-02-02 15:34:23 +0900

    UI
    UI 크기 조정
    asset 일부 적용

---
+++ Assets/Scripts/SwimTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwimTrigger : MonoBehaviour
+{
+    public bool m_isWater;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !m_isWater)
+        {
+            m_isWater = true;
+        }
+    }
+}
