440893dcd235b223e609a744ee98f92560095170
nandam1231 <yuco109@gmail.com>
2021-04-13 21:42:20 +0900

    underwater
    - remove pp v2
    - using volume in urp
    - add blur

---
+++ Assets/Scripts/underWater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Effects;
+using UnityEngine;
+
+public class underWater : MonoBehaviour
+{
+    public GameObject m_Camera;
+    public GameObject m_player;
+    private float m_blurSpread = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.fog = false; //close fog
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
