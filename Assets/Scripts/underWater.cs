54fac8bf869922350fca65fa267d87077d8ed964
nandam1231 <yuco109@gmail.com>
2021-04-14 15:03:36 +0900

    underWaterEffectFin
    - add Fog
    - revise depth

--- Assets/Scripts/underWater.cs
+++ Assets/Scripts/underWater.cs
@@ -7,17 +7,31 @@ public class underWater : MonoBehaviour
 {
     public GameObject m_Camera;
     public GameObject m_player;
-    private float m_blurSpread = 0.3f;
+    public SwimTrigger m_swimTrigger;
+    public GameObject m_GlobalVolume;
 
     // Start is called before the first frame update
     void Start()
     {
         RenderSettings.fog = false; //close fog
+        m_GlobalVolume.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //player가 물에 들어갔다면 fog -> true
+        if (m_swimTrigger.m_isWater)
+        {
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = new Color(0, 0.4f, 0.5f, 0.5f);    // 3 4 7
+            RenderSettings.fogDensity = 0.025f;
+            m_GlobalVolume.SetActive(true);
+        }
+        else
+        {
+            RenderSettings.fog = false;
+            m_GlobalVolume.SetActive(false);
+        }
     }
 }
