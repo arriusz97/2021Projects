db27623c3f2df2f719b63134c462e794f98bc0e0
namdam1231 <yuco109@gmail.com>
2021-04-21 19:40:42 +0900

    treeRendering
    -tree rendering revise
    -add seaweed on beach

---
+++ Assets/Scripts/terrainSetting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrainSetting : MonoBehaviour
+{
+    public float distance;
+    public Terrain m_terrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_terrain.treeDistance = distance;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
