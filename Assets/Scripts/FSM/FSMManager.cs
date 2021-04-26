5dc9915424efa172b309baaa440cad9ede1f09ff
namdam1231 <yuco109@gmail.com>
2021-04-26 19:40:06 +0900

    AttackAnim
    -AttackAnim add
    -monster fsm

---
+++ Assets/Scripts/FSM/FSMManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMManager : FSM<FSMManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InitState(this, FSMManager.)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FSM/IFSMState.cs
@@ -0,0 +1,12 @@
+﻿//각 상태의 처리 단계를 3단계로 처리
+public interface IFSMState<T>
+{
+    //상태 진입
+    void Enter(T e);
+
+    //상태 진행
+    void Execute(T e);
+
+    //상태 종료
+    void Exit(T e);
+}
