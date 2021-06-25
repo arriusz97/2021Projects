415f5ae866c0edc53b9d811db508a98cc0c46819
nandam1231 <58422027+nandam1231@users.noreply.github.com>
2021-06-25 19:40:21 +0900

    fishShaderFin
    -fish shader fin
    -fish school script 작성중

---
+++ Assets/Scripts/FishControl/FishSchoolCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSchoolCtrl : MonoBehaviour
+{
+    //To do list
+    //1. Bubble particle 설정해서 달아주기
+    //2. fish object list에 넣고 생성해주기 -> OBJ Pool 사용
+    //3. random path 설정해주기 -> wander function 생성
+    //4. avoid obstacle -> 더 공부해보기 // obstacle 만나면 좌/우로 방향 틀어서 다시 wander
+}
