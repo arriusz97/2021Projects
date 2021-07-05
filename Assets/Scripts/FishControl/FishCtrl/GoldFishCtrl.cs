29dec10ac62499a066e25b18f2c6668257ac1d50
nandam1231 <58422027+nandam1231@users.noreply.github.com>
2021-07-05 19:40:07 +0900

    fishpool
    -fish pool script fin
    -하이라이키에서 pool 연결해주기
    -area별로 fish 생성해주기

--- Assets/Scripts/FishControl/FishCtrl/GoldFishCtrl.cs
+++ Assets/Scripts/FishControl/FishCtrl/GoldFishCtrl.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class GoldFishCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("GoldFish 변수")]
+    private float m_Speed;
+    [SerializeField]
+    private float m_MaxSpeed = 7f;
+    [SerializeField]
+    private float m_MaxrotationSpeed = 0.9f;
+    //[SerializeField]
+    //private float neighbourDistance;
+    [SerializeField]
+    private int m_Boundary = 18;
+    private bool m_isTurning = false;
+
+    public Vector3 m_targetPosition = Vector3.zero;
+
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+
+    private GoldFishSpawnCtrl m_goldfishSpawnCtrl;
+
+
+    private void Start()
     {
-        
+        m_Speed = Random.Range(2f, 8f);
     }
 
-    // Update is called once per frame
-    void Update()
+    //init method 만들어서 연결
+    public void SpawnPos_Init(GoldFishSpawnCtrl spawnCtrl)
     {
-        
+        m_goldfishSpawnCtrl = spawnCtrl;
+    }
+
+    private void Update()
+    {
+        //change target position
+        GetTargetPosition();
+
+            if (Random.Range(0, 8) < 1)
+                setRotation();
+
+        transform.Translate(0, 0, Time.deltaTime * m_Speed);
+    }
+
+    void GetTargetPosition()
+    {
+        if (Random.Range(1, 80) < 50)
+        {
+            m_targetPosition = new Vector3(
+                Random.Range(-m_Boundary, m_Boundary),
+                Random.Range(-m_Boundary, m_Boundary),
+                Random.Range(-m_Boundary, m_Boundary)
+                );
+        }
+    }
+
+    void setRotation()
+    {
+        Vector3 direction = m_goldfishSpawnCtrl.transform.position - transform.position;
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(direction),
+                TurnSpeed() * Time.deltaTime);
+        }
+    }
+
+
+    float TurnSpeed()
+    {
+        return Random.Range(0.2f, m_MaxrotationSpeed);
     }
 }
