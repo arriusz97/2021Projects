59e8156a2a828c8622dce5c0e716cd730ab13a86
nandam1231 <yuco109@gmail.com>
2021-08-19 14:27:39 +0900

    soundCtrl
    -fadeout 구현 (아직 살짝 어색함)
    -Lobby terrain 표현 수정
    -Lobby bgm 삽입
    -8/20 footstep, 효과음 삽입 예정

---
+++ Assets/Scripts/SoundCtrl/LobbySoundCtrl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbySoundCtrl : MonoBehaviour
+{
+    public AudioSource m_Lobby_Bgm;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_Lobby_Bgm.Play();
+    }
+
+    
+}
