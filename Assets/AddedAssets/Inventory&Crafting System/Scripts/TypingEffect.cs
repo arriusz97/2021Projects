5bfffd0a45c6fc0f0c543e028b66838aa54388bc
arriusz97 <65055189+arriusz97@users.noreply.github.com>
2021-09-25 12:52:20 +0900

    TypingEffect

---
+++ Assets/AddedAssets/Inventory&Crafting System/Scripts/TypingEffect.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypingEffect : MonoBehaviour
+{
+
+    public Text ChatText; // 실제 채팅이 나오는 텍스트
+    //public Text CharacterName; // 캐릭터 이름이 나오는 텍스트
+
+
+    public List<KeyCode> skipButton; // 대화를 빠르게 넘길 수 있는 키
+
+    public string[] fulltext;
+
+    public string writerText = "";
+
+    bool isButtonClicked = false;
+
+    void Start()
+    {
+        StartCoroutine(TextPractice());
+    }
+
+    void Update()
+    {
+        foreach (var element in skipButton) // 버튼 검사
+        {
+            if (Input.GetKeyDown(element))
+            {
+                isButtonClicked = true;
+            }
+        }
+    }
+
+
+    IEnumerator NormalChat(string narrator, string narration)
+    {
+        int a = 0;
+        //CharacterName.text = narrator;
+        writerText = "";
+
+        //텍스트 타이핑 효과
+        for (a = 0; a < narration.Length; a++)
+        {
+            writerText += narration[a];
+            ChatText.text = writerText;
+            yield return null;
+        }
+
+        //키를 다시 누를 떄 까지 무한정 대기
+        while (true)
+        {
+            if (isButtonClicked)
+            {
+                isButtonClicked = false;
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TextPractice()
+    {
+        for (int i = 0; i < fulltext.Length; i++)
+        {
+            yield return StartCoroutine(NormalChat("캐릭터1", fulltext[i]));
+        }
+    }
+}
+
+
+//
+//            if (isButtonClicked)
+//            {
+//                ChatText.text = narration;
+//                a = narration.Length; // 버튼 눌리면 그냥 다 출력하게 함
+//                isButtonClicked = false;
+//            }
