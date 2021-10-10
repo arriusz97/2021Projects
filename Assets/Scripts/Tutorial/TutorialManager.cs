e82ec1eb9a3f182c5054d711a72f083bcdc10275
nandam1231 <yuco109@gmail.com>
2021-10-10 23:24:16 +0900

    StormCoroutine
    -storm coroutine 수정
    -tutorial particle 추가
    -tutorial 거의 끝

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -28,12 +28,15 @@ public class TutorialManager : MonoBehaviour
     [Header("Thunder")]
     [SerializeField]
     private GameObject[] m_Thunder;
+    [SerializeField]
+    private GameObject m_particle;
+
 
     public yachtCtrl m_yachtCtrl;
 
     [Header("Typing effect")]
   //  public TypingEffect m_tutorialTyping;
-    public TypingEffect_TMP m_tmpro_typing;
+    public TypingEffect_TMP m_Message01_typing;
     public TypingEffect_TMP m_Message02_typing;
 
     [Header("UI")]
@@ -47,20 +50,27 @@ public class TutorialManager : MonoBehaviour
     private GameObject m_Message02_background;
     [SerializeField]
     private Text m_Message02_text;
+
     private bool b_NarrationRunning = false;
+    public bool b_SceneChagne;  //scene을 변경할 bool 변수
 
     private void Start()
     {
-         //m_tutorialTyping.StartNarration();
-          m_tmpro_typing.StartNarration();
+          m_Message01_typing.StartNarration();
     }
 
-    public void sceneChange()
+    private void sceneChange()
     {
         m_Message02.gameObject.SetActive(true);
         m_Message02_background.gameObject.SetActive(true);
         m_Message02_outline.gameObject.SetActive(true);
         m_Message02_text.gameObject.SetActive(true);
+    }
+
+    //player가 storm trigger에 들어가면 부를 함수
+    public void Message02_func()
+    {
+        StartCoroutine(Message02_Start());
     }
 
     private void Update()
@@ -76,36 +86,44 @@ public class TutorialManager : MonoBehaviour
 
             RenderSettings.skybox = m_RainSkyBox;
             m_Light.color = Color.gray;
-            
-            sceneChange();
-
-            //typing이 끝났으면
-            if (!m_Message02_typing.m_isTyping)
-            {
-                Debug.Log("Typing 끝");
-                m_Message02.gameObject.SetActive(false);
-                m_Message02_background.gameObject.SetActive(false);
-                m_Message02_outline.gameObject.SetActive(false);
-                m_Message02_text.gameObject.SetActive(false);
-            }
-            
-
+            m_particle.gameObject.SetActive(false);
         }
 
         //천둥이 치고 15초가 지났다면
-        if (m_yachtCtrl.b_SceneChagne)
-        { 
-
+        if (b_SceneChagne)
+        {
+            m_yachtCtrl.m_StormSound.volume = 1.0f;
             m_SceneChange_Image.gameObject.SetActive(true);
             SceneManager.LoadScene(2);
         }
     }
 
-    IEnumerator SceneChange()
+    IEnumerator Message02_Start()
     {
-        yield return null;
+        //폭풍우 치고 3초 뒤 나레이션02 시작
+        WaitForSeconds three = new WaitForSeconds(3.0f);
+        yield return three;
+
+        Debug.Log("Coroutine 시작하고 3초 지남");
+
+        //message02 set active true
+        sceneChange();
 
         //한번만 발동되게 하기
         m_Message02_typing.StartNarration();
+        Debug.Log("message 02 시작");
+
+        StartCoroutine(SceneChange());
+    }
+
+    //storm이 시작되고, player가 배가 조종되지 않는 다는 것을 알게된 후 배 위를 돌아다니다가
+    //15초 뒤 천둥이 요트 위에 치게 되고 Scene change
+    IEnumerator SceneChange()
+    {
+        Debug.Log("SceneChange Coroutine 불려짐");
+        WaitForSeconds fift = new WaitForSeconds(15.0f);
+        yield return fift;
+
+        b_SceneChagne = true;
     }
 }
