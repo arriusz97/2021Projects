73d0844392189710cf23bf4404b1abacb72074c8
arriusz97 <65055189+arriusz97@users.noreply.github.com>
2021-10-16 21:50:47 +0900

    Control Guide Script1

--- Assets/AddedAssets/Inventory&Crafting System/Scripts/ControlGuide.cs
+++ Assets/AddedAssets/Inventory&Crafting System/Scripts/ControlGuide.cs
@@ -8,7 +8,8 @@ public class ControlGuide : MonoBehaviour
     [SerializeField]
     private TypingEffect typingEffect;
 
-
+    [SerializeField]
+    private int[] narrationSet = new int[10];
 
     public int[] guideBoolean = Enumerable.Repeat<int>(0, 10).ToArray<int>();
 
@@ -17,7 +18,7 @@ public class ControlGuide : MonoBehaviour
     {
         if (guideBoolean[0] == 0)
         {
-            typingEffect.StartNarration(0, 2);
+            typingEffect.StartNarration(0, narrationSet[0]);
         }
         else if(guideBoolean[9] == 1)
         {
@@ -36,17 +37,17 @@ public class ControlGuide : MonoBehaviour
                     typingEffect.Skip();
                     guideBoolean[0] = 1;
                     Debug.Log("move guide clear");
-                    typingEffect.StartNarration(3, 4);
+                    typingEffect.StartNarration(narrationSet[0]+1, narrationSet[1]);
                 }
             }
             else if (guideBoolean[1] == 0)
             {
-                if (Input.GetKeyDown(KeyCode.LeftShift))
+                if (Input.GetKey(KeyCode.LeftShift) && Input.inputString.Length == 1 && Input.inputString[0] == 'W')                  
                 {
                     typingEffect.Skip();
                     guideBoolean[1] = 1;
                     Debug.Log("run guide clear");                   
-                    typingEffect.StartNarration(5, 5);
+                    typingEffect.StartNarration(narrationSet[1] + 1, narrationSet[2]);
                 }
             }
         }
