9b7f0152863a2ea538808794dac507d8d3dd8100
arriusz97 <65055189+arriusz97@users.noreply.github.com>
2021-10-29 13:15:13 +0900

    DeadUI Update

--- Assets/Scripts/IngameCtrl.cs
+++ Assets/Scripts/IngameCtrl.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class IngameCtrl : MonoBehaviour
 {
     [SerializeField]
-    private CanvasGroup deadUICanvas;
+    private CanvasGroup deadUICanvas, gui;
     
     public void playerDead()
     {
@@ -22,11 +22,12 @@ public class IngameCtrl : MonoBehaviour
     //player가 죽고나서 5초 뒤 lobby로 전환
     IEnumerator dead()
     {
+        
         yield return StartCoroutine(Fade(true));
 
         WaitForSeconds five  = new WaitForSeconds(5.0f);
         yield return five;
-
+        gui.alpha = 1;
         SceneManager.LoadScene(0);
     }
 
@@ -41,6 +42,7 @@ public class IngameCtrl : MonoBehaviour
     private IEnumerator Fade(bool isFadeIn) 
     { 
         float timer = 0f;
+        gui.alpha = 0;
         while (timer <= 1f) 
         { 
             yield return null;
@@ -48,8 +50,9 @@ public class IngameCtrl : MonoBehaviour
             deadUICanvas.alpha = Mathf.Lerp(isFadeIn ? 0 : 1, isFadeIn ? 1 : 0, timer); 
         }
         if (!isFadeIn) 
-        { gameObject.SetActive(false); 
-        } 
+        { 
+            gameObject.SetActive(false); 
+        }
     }
 
 }
