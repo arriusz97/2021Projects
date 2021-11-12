a9ad5c2877d58015e12493d22eca1962a79e698d
nandam1231 <58422027+nandam1231@users.noreply.github.com>
2021-11-12 18:51:17 +0900

    Tooltipbug_fix
    - tooltip 잔떨림 버그 수정
    - tutorial scene 에서 esc 버튼을 누르면 titlescene으로 가게 설정

--- Assets/AddedAssets/Inventory&Crafting System/Scripts/Tooltip.cs
+++ Assets/AddedAssets/Inventory&Crafting System/Scripts/Tooltip.cs
@@ -19,8 +19,17 @@ public class Tooltip : MonoBehaviour
     public void ShowToolTip(ItemObject _item, Vector3 _pos)
     {
         go_Base.SetActive(true);
-        _pos += new Vector3(go_Base.GetComponent<RectTransform>().rect.width * 0f,
-                            -go_Base.GetComponent<RectTransform>().rect.height * 0.5f, 0);
+
+        if (_item.name == "Recipe Campfire" || _item.name == "Recipe Shelter" || _item.name == "Recipe Tent")
+        {
+            _pos += new Vector3(go_Base.GetComponent<RectTransform>().rect.width * 0f,
+                                -go_Base.GetComponent<RectTransform>().rect.height * 0.95f, 0);
+        }
+        else
+        {
+            _pos += new Vector3(go_Base.GetComponent<RectTransform>().rect.width * 0f,
+                                -go_Base.GetComponent<RectTransform>().rect.height * 0.55f, 0);
+        }
         go_Base.transform.position = _pos;
 
         txt_ItemName.text = _item.data.Name;
